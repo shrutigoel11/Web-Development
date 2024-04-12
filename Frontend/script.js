fb0e6a5c07cac85ed6cc314da2f35735b76f4f05
shrutigoel11 <shrutigoel0111@gmail.com>
2024-04-12 23:29:09 +0530

    updated folder

---
+++ Frontend/script.js
@@ -0,0 +1,15 @@
+console.log('Javascript');
+alert("Tutorial");
+// dir prints properties and methods 
+
+console.dir(window.document);
+let getid=document.getElementById("myid");
+let getclass=document.getElementsByClassName("myclass");
+let gettag=document.getElementsByTagName("button");
+console.log(getid);
+console.log(getclass);
+console.log(gettag);
+
+let elements=document.querySelector("p");
+console.dir(elements);
+console.log(getid.tagName);
